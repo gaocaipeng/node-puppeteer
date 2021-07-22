5bd3975ddf50ccac1a3ccd72325a57531bc259fb
高彩鹏 <gaocaipeng@u-bx.com>
2021-07-22 13:18:55 +0800

    增加端口号

---
+++ config/app.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: 高彩鹏
+ * @Date: 2020-03-09 16:50:01
+ * @LastEditors: 高彩鹏
+ * @LastEditTime: 2020-09-03 18:07:49
+ */
+const app = {
+  port: '3001'
+}
+
+module.exports = app
